15b949c299049205a7640f24be8a4b6b526618c8
Yann Depledt <100939164+ydepledt@users.noreply.github.com>
2026-08-20 14:02:35 +0200

    Expose publisher API at package root

--- src/video_builder_publisher/__init__.py
+++ src/video_builder_publisher/__init__.py
@@ -1,5 +1,18 @@
 """Shared video building and publication primitives."""
 
+from .publishing import (
+    InstagramConfig,
+    InstagramPublisher,
+    PublishError,
+    PublishResult,
+    Publisher,
+    TikTokConfig,
+    TikTokPublisher,
+    YouTubeConfig,
+    YouTubePublisher,
+    authorize_youtube,
+    build_publisher,
+)
 from .queue import PublishQueue
 from .store import PublicationStore, RunRecord
 from .video import (
@@ -15,12 +28,23 @@ from .video import (
 
 __all__ = [
     "VIDEO_PROFILES",
+    "InstagramConfig",
+    "InstagramPublisher",
+    "PublishError",
     "PublishQueue",
+    "PublishResult",
     "PublicationStore",
+    "Publisher",
     "RawVideoEncoder",
     "RunRecord",
+    "TikTokConfig",
+    "TikTokPublisher",
     "VideoSpec",
+    "YouTubeConfig",
+    "YouTubePublisher",
     "atomic_video_target",
+    "authorize_youtube",
+    "build_publisher",
     "mux_audio_track",
     "probe_video",
     "resolve_video_spec",
